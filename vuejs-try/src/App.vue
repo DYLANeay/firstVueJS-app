4fdad322bfdd5427358befca1f3a03d7e80236e6
DYLANeay <dylan.eray6@gmail.com>
2025-05-07 16:16:57 +0200

    easy codes examples

--- vuejs-try/src/App.vue
+++ vuejs-try/src/App.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>Compteur : {{ count }}</p>
+  <p
+    v-bind:id="`p-${count}`"
+    v-bind:style="{ color: count > 5 ? 'green' : 'red' }"
+  >
+    Compteur : {{ count }} <div v-html="firstName"></div>
+  </p>
+  <div v-if="count >= 5">Bravo, vous avez clické plus de 5x</div>
+  <div v-else>Vous avez clické moins de 5x</div>
   <button @click="increment">Incrémenter</button>
   <button @click="decrement">Décrementer</button>
 </template>
@@ -7,6 +14,7 @@
 <script setup>
 import { ref } from 'vue';
 
+const firstName = '<span>Dylan</span>';
 const count = ref(0);
 console.log(count, count.value);
 const increment = (e) => {
